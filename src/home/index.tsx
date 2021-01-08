cb2c9e05a911a4b2ba8c5a4d931fcf975152b2bf
Tim Remnant <timwebdesign2013@gmail.com>
2021-01-08 14:43:52 +0000

    Swapping dark mode colors

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,14 +1,38 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { notesSelector } from "../store/notes/notesSlice";
+import { Note } from "../store/notes/notesTypes";
+import { NoteItem } from './item';
+import { History } from 'history';
 
 export const Home = (): JSX.Element => {
 
     const notes = useSelector(notesSelector);
-    console.log(notes);
+    const history = useHistory();
+    const noteList = buildNoteList(notes, history);
 
     return (
-        <div className="max-w-4xl flex items-center h-screen h-auto flex-wrap mx-auto">
-
+        <div className="h-auto h-screen mx-auto">
+            <div className="grid grid-cols-8 gap-4 p-4">
+                {noteList}
+            </div>
         </div>
     );
+}
+
+const buildNoteList = (blogs: Note[], history: History<unknown>): JSX.Element[] => {
+    const elements: JSX.Element[] = [];
+    blogs.forEach((note: Note, index: number) => {
+
+        const onClickHandler = (): void => {
+            history.push('note/' + note.id);
+        };
+
+        elements.push(<NoteItem
+            title={note.title}
+            key={index}
+            onClick={onClickHandler}
+        />);
+    });
+    return elements;
 }
